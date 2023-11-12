6a35f8d3ffb2d761a6a881c25159f953cdf9baeb
InesT7 <mitorres@gus.pittstate.edu>
2023-11-12 01:23:03 -0600

    addList

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 
 /* GET users listing. */
 router.get('/list', function(req, res, next) {
-  res.send('Here is the list');
+  res.send('Here i');
 });
 
 
